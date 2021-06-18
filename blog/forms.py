2c01d251d782b59aec622d493d91377fff327d9f
Shasa <shasa.thuo@gmail.com>
2021-06-18 06:06:40 +0300

    Styled the email page

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,13 @@
 from django import forms 
 
 class EmailPostForm(forms.Form):
-    name =  forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
+    name =  forms.CharField(max_length=25, widget=forms.TextInput(
+                attrs={'placeholder': "Name of sender..."}
+    ))
+    email = forms.EmailField(widget=forms.TextInput(
+                attrs={'placeholder': "Sender email..."}
+    ))
+    to = forms.EmailField(widget=forms.TextInput(
+                attrs={'placeholder': "Recepient email..."}
+    ))
     comments = forms.CharField(required=False, widget=forms.Textarea)
