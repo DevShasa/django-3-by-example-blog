3054b76b59e3510f7de6baad60171fefafd98eef
Shasa <shasa.thuo@gmail.com>
2021-06-26 20:09:01 +0300

    it wirks dont touch anything!!

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post, Comment
 
+
 # Register your models here.
 @admin.register(Post) # this decorator is similar to admin.site.register()
 class PostAdmin(admin.ModelAdmin):
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.views.generic import  ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
+
 
 # Create your views here.
 # Views receive web request and return web response 
 # Each view renders a template, passing variables to it
-
-class PostListView(ListView):
-
-    # Default: model = Post
-    queryset = Post.published.all()
-    # Default: post_list
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
-
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, 
@@ -43,13 +36,28 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
     
+    # Recooment similar posts
+    # Get a python [list] of ids of tags for the current post...
+    # values_list() queryset returns tuples with the values of the given fields...
+    # flat=True is to get the list [1, 2, 3, ...] instead of [(1,), (2,), (3,) ...] 
+    post_tag_ids = post.tags.values_list("id", flat=True)
+    # Fetch all posts that have the tags in the post_tag_ids queryset
+    similar_posts = Post.published.filter(tags__in=post_tag_ids).exclude(id=post.id)
+    # Use Count to generate a value theat represents number of shared tags
+    # Order the results by the number of shared tags in -descending-order...
+    # Order the resulting queryset above by published so that recent posts come first...
+    # Slice the result to onlu get four entries 
+    # for more info on annotate check page 127 in django documentation book     
+    similar_posts = similar_posts.annotate(same_tags =Count("tags")).order_by('-same_tags', "-publish")[:4]
+
     return render(request,"blog/post/detail.html",
                 # Context
                 {
                 "post": post, # The blogpost   
                 "comments": comments, # The comments under the blog
                 "new_comment": new_comment, # The comment just submitted
-                "comment_form": comment_form # The modelForm object
+                "comment_form": comment_form, # The modelForm object
+                "similar_posts": similar_posts
                 }) 
                 
 # This handles the form 
@@ -91,20 +99,45 @@ def post_share(request, post_id):
 
 
 '''This is the old listview, which is similar to PostListView above'''
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-# def post_list(request):
-#     object_list = Post.published.all() # Here we use our custom object 
-#     # Alternatively Post.object.filter(status="published")
-#     paginator = Paginator(object_list, 3) # Instantiate paginator class, display three posts on every page
-#     page = request.GET.get('page') # Get the page parameter, which indicates current page number
-#     try:
-#         # Obtain the objects of the desired page by calling page() method of paginator 
-#         posts = paginator.page(page)
-#     except  PageNotAnInteger:
-#         # If page is not an integer deliver the first page
-#         # When you  load blog/ this line will be rendered
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # If page is oyut of range, deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', {'posts':posts})
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all() # Here we use our custom object 
+    # Alternatively Post.object.filter(status="published")
+
+    tag = None
+    if tag_slug:
+
+        # Fetch the tag from the database 
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        # Get only the posts that have the indicated tag 
+        object_list = object_list.filter(tags__in=[tag]) 
+
+    # Instantiate paginator class, display three posts on every page
+    paginator = Paginator(object_list, 3) 
+    # Get the page parameter, which indicates current page number
+    page = request.GET.get('page') 
+    try:
+        # Obtain the objects of the desired page by calling page() method of paginator 
+        posts = paginator.page(page)
+    except  PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        # When you  load blog/ this line will be rendered
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is oyut of range, deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {
+                                                    'posts':posts,
+                                                    'tag':tag,
+                                                    'page':page,
+                                                    })
+
+
+# from django.views.generic import  ListView
+# class PostListView(ListView):
+
+#     # Default: model = Post
+#     queryset = Post.published.all()
+#     # Default: post_list
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
